621c7e630d188b6bc6c5a57dcf3d0d53a33219cc
jishnu-NKS-Tec <jishnunarayanan378@gmail.com>
2026-09-03 16:48:46 +0530

    changes

--- 21fiftyone/app/page.tsx
+++ 21fiftyone/app/page.tsx
@@ -3,10 +3,8 @@ import Script from "next/script";
 import About1 from "@/component/About1";
 import AboutSection from "@/component/Aboutsection";
 
-
-import  BenefitSection  from "@/component/BenefitSection";
+import BenefitSection from "@/component/BenefitSection";
 import BreakTheMold from "@/component/Breakthemold";
-
 
 import Features from "@/component/Features";
 import FloatingNavbar from "@/component/Floatingnavbar";
@@ -22,7 +20,7 @@ const NAV_ITEMS = [
   { label: "Archive",     ariaLabel: "View Archive",     link: "/archive"     },
   { label: "Studio",      ariaLabel: "View Studio",      link: "/studio"      },
 ];
- 
+
 const SOCIAL_ITEMS = [
   { label: "Instagram", link: "https://instagram.com/21fiftyone" },
   { label: "LinkedIn",  link: "https://linkedin.com/company/21fiftyone" },
@@ -38,45 +36,31 @@ const localBusinessJsonLd = {
   "image": "https://21fiftyone.com/logo/2151-logo.png",
   "@id": "https://21fiftyone.com/",
   "url": "https://21fiftyone.com/",
-  "telephone": "+91 82816 10051",
+  "telephone": "077364 02151",
   "address": {
     "@type": "PostalAddress",
-    "streetAddress": "Regus Door No. 2703, Cabin 721, HiLITE Business Park, 7th Floor, Tower 2, Pantheeramkavu",
+    "streetAddress": "Pantheeramkavu",
     "addressLocality": "Kozhikode",
     "postalCode": "673014",
     "addressCountry": "IN"
   },
   "geo": {
     "@type": "GeoCoordinates",
-    "latitude": 11.247482,
-    "longitude": 75.833709
-  },
-  "openingHoursSpecification": {
-    "@type": "OpeningHoursSpecification",
-    "dayOfWeek": [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ],
-    "opens": "10:00",
-    "closes": "18:00"
-  },
-  "sameAs": [
-    "https://www.instagram.com/21fiftyone?igsh=MXV2NTI3M2QzMTMwZw==",
-    "https://www.facebook.com/share/1Aw4MkQKzk/?mibextid=wwXIfr"
-  ]
+    "latitude": 11.2474739,
+    "longitude": 75.8337082
+  }
 };
 
 const organizationJsonLd = {
   "@context": "https://schema.org",
   "@type": "Organization",
   "name": "21fiftyone",
-  "alternateName": "21fiftyone",
   "url": "https://21fiftyone.com/",
-  "logo": "https://21fiftyone.com/logo/2151-logo.png"
+  "logo": "https://21fiftyone.com/logo/2151-logo.png",
+  "sameAs": [
+    "https://www.facebook.com/share/1Aw4MkQKzk/?mibextid=wwXIfr",
+    "https://www.instagram.com/21fiftyone?igsh=MXV2NTI3M2QzMTMwZw=="
+  ]
 };
 
 const breadcrumbJsonLd = {
@@ -85,13 +69,13 @@ const breadcrumbJsonLd = {
   "itemListElement": [{
     "@type": "ListItem",
     "position": 1,
-    "name": "Services",
-    "item": "https://21fiftyone.com/services"
-  },{
+    "name": "about",
+    "item": "https://21fiftyone.com/about"
+  }, {
     "@type": "ListItem",
     "position": 2,
-    "name": "Contact",
-    "item": "https://21fiftyone.com/contact"
+    "name": "services",
+    "item": "https://21fiftyone.com/services"
   }]
 };
 
@@ -119,33 +103,25 @@ export default function Home() {
       />
 
       {/* <SectionBug/> */}
-     
-        <FloatingNavbar />
-        <HeroSection />
-        {/* <SocialFloat  /> */}
-         {/* <CraftingCulture /> */}
-         <About1/> 
 
-          <AboutSection />
-           
-          <Features/>
-       
-        
-        <BenefitSection />
-        {/* <StickyVideoSection /> */}
-      
-     
+      <FloatingNavbar />
+      <HeroSection />
+      {/* <SocialFloat  /> */}
+      {/* <CraftingCulture /> */}
+      <About1 />
 
-       
-         
+      <AboutSection />
 
-          <ProjectsScroll />
-      
-    
-        <BreakTheMold />
-        <Footer />
-       
-   
+      <Features />
+
+      <BenefitSection />
+      {/* <StickyVideoSection /> */}
+
+      <ProjectsScroll />
+
+      <BreakTheMold />
+      <Footer />
+
     </div>
   );
 }
